623fab44143ddebb3d4590de6d6b3f7f0da748ac
valerc123 <valentina.rua123@gmail.com>
2021-04-13 01:24:06 -0500

    fix version sdk

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -3,8 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-            int number = 972;
-            
-            Console.WriteLine("{0} Mission Street", number);
+        int number = 972;
+        
+        Console.WriteLine("{0} Mission Street", number);
     }
 }
