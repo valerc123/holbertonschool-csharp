93a3d4dd960d29acfefbff5d4e03abae11ea5d84
valerc123 <valentina.rua123@gmail.com>
2021-04-13 23:41:54 -0500

    Print alphabet except q and e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(byte a = 97; a <= 122; a++)
+            {
+                if (a == 113  || a ==  101){
+                    continue;
+                }else{
+                    Console.Write((char)a);
+                }
+            }
+        }
+    }
+}
