8044b3d255b50cd177241b40b08cf4a7dbca3ea1
valerc123 <valentina.rua123@gmail.com>
2021-04-13 23:53:31 -0500

    print hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (num <= 98){
+                Console.Write("{0} = 0x{0:x} \n", num);
+                num++;
+            }
+        }
+    }
+}
