6589f9961d6c020e89832e0d0d5184308735d08e
valerc123 <valentina.rua123@gmail.com>
2021-04-14 01:51:23 -0500

    fizz buzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0){
+                    Console.Write("Buzz ");
+                }
+                else if (i % 3 == 0){
+                    if (i % 5 == 0){
+                        Console.Write("FizzBuzz ");
+                    }
+                    Console.Write("Fizz ");
+                }else{
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
