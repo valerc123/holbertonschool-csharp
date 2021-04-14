72b2ac1487112c4cf86fdac24e50421a470f18d5
valerc123 <valentina.rua123@gmail.com>
2021-04-13 23:29:29 -0500

    Print alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("abcdefghijklmnopqrstuvwxy");
+        }
+    }
+}
