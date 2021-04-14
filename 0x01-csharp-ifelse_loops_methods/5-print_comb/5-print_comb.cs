ea6dbe682fdea592ae948adb14b68aa9dbbb3985
valerc123 <valentina.rua123@gmail.com>
2021-04-14 00:05:12 -0500

    prints numbers from 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while(num <= 98){
+                Console.Write("{0:00}, ", num);
+                num++;
+            }
+            Console.Write("99\n");
+        }
+    }
+}
