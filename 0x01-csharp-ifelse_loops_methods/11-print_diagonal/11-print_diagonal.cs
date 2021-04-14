8179dc34bb59944b65604da49c778a3bd025fc95
valerc123 <valentina.rua123@gmail.com>
2021-04-14 01:34:34 -0500

    Print diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if(length <= 0){
+            Console.WriteLine();
+        }else{
+            for (int slash = 1; slash <= length; slash++)
+            {
+                for (int spaces = 0; spaces < slash; spaces++){
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
