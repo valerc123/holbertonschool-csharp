d2d8ee99116ef5b227aa59f8a747d5fd1bf17cd4
valerc123 <valentina.rua123@gmail.com>
2021-04-14 00:22:42 -0500

    Print comb 2

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if(i < j){
+                        Console.Write("{0}{1}, ", i, j);
+                    }
+                }
+            }
+            Console.WriteLine("89");
+        }
+    }
+}
