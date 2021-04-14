e10bea423d463ecc36ee9e384a2d2db4b73afaa3
valerc123 <valentina.rua123@gmail.com>
2021-04-14 00:42:10 -0500

    is lowerCase

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c){
+        if ((byte)c >= 97 && (byte)c <= 122){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
