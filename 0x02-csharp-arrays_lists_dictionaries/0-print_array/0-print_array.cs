3f1f95da38dfe4951e9e1951fd0201a921b281b9
valerc123 <valentina.rua123@gmail.com>
2021-04-15 01:07:54 -0500

    add condition for negative size

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,13 +4,19 @@ class Array
 {
     public static int[] CreatePrint(int size)
     {
-        int[] new_arr = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            new_arr[i] = i;
-            Console.Write("{0}{1}", i, i == (size - 1) ? "" : " ");
+        if(size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }else {
+            int[] new_arr = new int[size];
+            
+            for (int i = 0; i < size; i++)
+            {
+                new_arr[i] = i;
+                Console.Write("{0}{1}", i, i == (size - 1) ? "" : " ");
+            }
+            Console.WriteLine();
+            return new_arr;
         }
-        Console.WriteLine();
-        return new_arr;
     }
 }
