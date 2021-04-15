084915506e1099400760205596ff733c1e4befc9
valerc123 <valentina.rua123@gmail.com>
2021-04-15 01:51:57 -0500

    add C.W index out of range

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -5,6 +5,7 @@ class Array
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
         if (index > array.Length -1 || index < 0){
+            Console.WriteLine("Index out of range");
             return array;
         }else{
             array[index] = n;
