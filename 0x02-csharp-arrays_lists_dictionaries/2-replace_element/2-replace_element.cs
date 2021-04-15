cbfb361b8e8445f0f7d72c52b526d11bdf587870
valerc123 <valentina.rua123@gmail.com>
2021-04-15 01:47:30 -0500

    Replace Element

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index > array.Length -1 || index < 0){
+            return array;
+        }else{
+            array[index] = n;
+            return array;
+        }
+    }
+}
