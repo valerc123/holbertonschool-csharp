62513b17d26f67446148d2999b65f852b3b4091b
valerc123 <valentina.rua123@gmail.com>
2021-04-15 00:51:45 -0500

    Print Array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int[] new_arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            new_arr[i] = i;
+            Console.Write("{0}{1}", i, i == (size - 1) ? "" : " ");
+        }
+        Console.WriteLine();
+        return new_arr;
+    }
+}
