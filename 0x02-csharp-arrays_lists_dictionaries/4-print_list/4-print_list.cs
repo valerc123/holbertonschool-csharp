aa079ebe23d90e475c677e85fad89fce0da0148a
valerc123 <valentina.rua123@gmail.com>
2021-04-15 02:52:31 -0500

    Print list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> new_list = new List<int>(size);
+        if (size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else if(size == 0){
+            Console.WriteLine();
+        }
+        else{
+            for (int i = 0; i < size; i++)
+            {
+                new_list.Add(i);
+                Console.Write("{0}{1}", i, i < size -1 ? " " : "\n");
+            }
+        }
+        return new_list;
+    }
+}
