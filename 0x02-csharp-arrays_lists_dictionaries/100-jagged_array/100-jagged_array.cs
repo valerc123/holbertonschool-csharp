8948d6187d57745af9d95bd97d5b0f87aa3385c2
valerc123 <valentina.rua123@gmail.com>
2021-04-14 23:59:52 -0500

    jagged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [][] jagged = new int[][]
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0, 1}
+        };
+        for (int x = 0; x < jagged.Length; x++) {
+            for (int y = 0; y < jagged[x].Length; y++) {
+                Console.Write("{0}{1}", jagged[x][y], y == (jagged[x].Length -1) ? "" : " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
