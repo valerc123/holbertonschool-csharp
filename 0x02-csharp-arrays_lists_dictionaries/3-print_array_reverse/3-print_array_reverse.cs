b48cd48cbe0b7192bf03f4db24f0d64d4ece5d05
valerc123 <valentina.rua123@gmail.com>
2021-04-15 02:24:13 -0500

    print reverse

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,7 +4,7 @@ class Array
 {
     public static void Reverse(int[] array)
     {
-        if (array != null || array.Length > 0){
+        if (array != null && array.Length > 0){
             for (int i = array.Length -1; i >= 0; i--)
             {
                 Console.Write("{0}{1}",array[i], i -1 >=0 ? " " : "\n");
