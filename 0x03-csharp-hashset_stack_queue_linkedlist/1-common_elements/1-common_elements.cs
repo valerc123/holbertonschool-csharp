b3d00f547b2575490196d2fa0a50218368af9eb8
valerc123 <valentina.rua123@gmail.com>
2021-04-18 14:35:40 -0500

    common elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> common = list1.FindAll(elem => list2.Contains(elem));
+        return common;
+    }
+}
