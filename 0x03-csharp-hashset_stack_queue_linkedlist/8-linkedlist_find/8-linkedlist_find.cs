87aea98ccbabe99bc80692c908b4c1c7a5415124
valerc123 <valentina.rua123@gmail.com>
2021-04-18 18:57:30 -0500

    linkedlist find

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+        foreach (var elem in myLList)
+        {
+            if(elem == value){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+}
