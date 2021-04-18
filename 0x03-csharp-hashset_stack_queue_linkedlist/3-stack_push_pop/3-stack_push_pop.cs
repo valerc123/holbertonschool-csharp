a67e4b08a9eb3243d99d38a0c6ad05d04b0574e0
valerc123 <valentina.rua123@gmail.com>
2021-04-18 17:31:58 -0500

    Stack push pop

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -7,13 +7,19 @@ class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: {0}", aStack.Count);
-        Console.WriteLine("Top item: {0}", aStack.Peek());
         if(aStack.Count == 0){
             Console.WriteLine("Stack is empty");
+        }else {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
         }
         if(aStack.Contains(search)){
-            Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
-            aStack.Clear();
+            Console.WriteLine("Stack contains \"{0}\": True", search);
+            while (aStack.Contains(search))
+            {
+                aStack.Pop();
+            }
+        }else {
+            Console.WriteLine("Stack contains \"{0}\": False", search);
         }
         aStack.Push(newItem);
         return aStack;
