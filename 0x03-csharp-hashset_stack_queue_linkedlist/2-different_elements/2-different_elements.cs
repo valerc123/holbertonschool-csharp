d1644f52306f335026b1cd19031afa76763813bf
valerc123 <valentina.rua123@gmail.com>
2021-04-18 15:10:04 -0500

    Different Elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> sorted = new List<int>();
+
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (!list2.Contains(list1[i])){
+                sorted.Add(list1[i]);
+            }
+        }
+        for (int j = 0; j < list2.Count; j++){
+
+            if (!list1.Contains(list2[j])){
+                sorted.Add(list2[j]);
+            }
+        }
+        sorted.Sort();
+        return sorted;
+    }
+}
