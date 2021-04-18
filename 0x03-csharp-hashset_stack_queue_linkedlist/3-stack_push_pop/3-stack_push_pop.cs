2383544eeeb4df76a56833744d16de9044ca224f
valerc123 <valentina.rua123@gmail.com>
2021-04-18 17:20:44 -0500

    Stacks

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        Console.WriteLine("Top item: {0}", aStack.Peek());
+        if(aStack.Count == 0){
+            Console.WriteLine("Stack is empty");
+        }
+        if(aStack.Contains(search)){
+            Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
+            aStack.Clear();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
