546826acd152e9bc7aec8fbb5ce75d8c052a72e6
valerc123 <valentina.rua123@gmail.com>
2021-04-18 14:10:15 -0500

    unique add

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        int[] unique = myList.Distinct().ToArray();
+        foreach(int num in unique){
+            sum = sum + num;
+        }
+        return sum;
+    }
+}
