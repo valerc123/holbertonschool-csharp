88e61d28435347a83c1cdd7df9eb8eaff22aee7e
valerc123 <valentina.rua123@gmail.com>
2021-04-18 18:39:51 -0500

    linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> new_node = new LinkedListNode<int>(n);
+
+        myLList.AddFirst(n);
+        return new_node;
+    }
+}
