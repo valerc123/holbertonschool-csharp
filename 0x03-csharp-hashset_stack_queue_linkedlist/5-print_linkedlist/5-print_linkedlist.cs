1a0224a9a2273d0563ad2835186e2b26349c90a4
valerc123 <valentina.rua123@gmail.com>
2021-04-18 18:29:05 -0500

    linkend list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> new_linkedList = new LinkedList<int>();
+        for (int i = 0; i < size; i++)
+        {
+            new_linkedList.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return new_linkedList;
+    }
+}
