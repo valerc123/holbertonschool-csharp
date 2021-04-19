ce48a40d36d25d1f70d0694d50a2b2b7e3db91e7
valerc123 <valentina.rua123@gmail.com>
2021-04-18 23:21:03 -0500

    divisible by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> result = new List<bool>();
+
+        foreach (var num in myList)
+        {
+            if(num%2==0){
+                result.Add(true);
+            }else{
+                result.Add(false);
+            }
+        }
+        return result;
+    }
+}
