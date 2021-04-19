6be6df88b3e7d297e201b640fd73e81744a09192
valerc123 <valentina.rua123@gmail.com>
2021-04-18 21:24:41 -0500

    insert node linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> new_node = new LinkedListNode<int>(n);
+        int value = n;
+        LinkedListNode<int> node = myLList.Find(16);
+        myLList.AddAfter(node, n);
+        
+        return new_node;
+    }
+}
