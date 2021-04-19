5aa8aa1590bff52cfc2a0be6dde9f6a67a72324a
valerc123 <valentina.rua123@gmail.com>
2021-04-18 23:10:40 -0500

    max integer

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        int max = 0;
+        if (myList.Count == 0){
+            Console.WriteLine("List is empty");
+            return -1;
+        }else {
+            foreach (var item in myList)
+            {
+                if (max<item){
+                    max = item;
+                }
+            }
+        }
+        return max;
+    }
+}
