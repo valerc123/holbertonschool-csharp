36c461f3593f7748254caaa69304e82143f3a528
valerc123 <valentina.rua123@gmail.com>
2021-04-18 23:13:57 -0500

    max integer

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -8,7 +8,7 @@ class List
         int max = 0;
         if (myList.Count == 0){
             Console.WriteLine("List is empty");
-            return -1;
+            return (-1);
         }else {
             foreach (var item in myList)
             {
