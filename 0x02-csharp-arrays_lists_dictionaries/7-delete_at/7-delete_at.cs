6142cbe8289d0f540b21a67ae16b742d216eedbf
valerc123 <valentina.rua123@gmail.com>
2021-04-18 23:47:18 -0500

    delete

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -5,13 +5,12 @@ class List
 {
     public static List<int> DeleteAt(List<int> myList, int index)
     {
-        if(iindex > myList.Count - 1 || index < 0){
+        if (myList.Count - 1 < index || index < 0){
             Console.WriteLine("Index is out of range");
             return myList;
         }
-        else {
-           myList.Remove(myList[index]);
-            return myList;
-        }
+        myList.Remove(myList[index]);
+        return myList;
+        
     }
 }
