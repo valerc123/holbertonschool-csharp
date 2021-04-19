1748c2f9f27323677e1e6ac9b67caa348f7af1c8
valerc123 <valentina.rua123@gmail.com>
2021-04-18 20:29:00 -0500

    linked list get node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach (var node in myLList)
+        {
+            if (index == n){
+                return node;
+            }
+            index++;
+        }
+        return 0;
+    }
+}
