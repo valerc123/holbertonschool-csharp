323f97f0b0172173660fce235ffcc240b846ab91
valerc123 <valentina.rua123@gmail.com>
2021-04-19 00:07:51 -0500

    number keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+        foreach (var item in myDict)
+        {
+            count++;
+        }
+        return count;
+    }
+}
