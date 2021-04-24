363fbc50b8c833ab54c7a8571a8507708c1d2936
valerc123 <valentina.rua123@gmail.com>
2021-04-23 20:51:14 -0500

    best score

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,20 +6,21 @@ class Dictionary
     public static string BestScore(Dictionary<string, int> myList)
     {
         int big = 0;
-        string key_big = string.Empty;
+        string key_big = "";
         if (myList.Count == 0)
         {
             return "None";
-        }
-        foreach (var item in myList)
-        {
-            if (item.Value > big){
-                big = item.Value;
-                key_big = item.Key;
-            }else{
-                continue;
+        }else {
+            foreach (var item in myList)
+            {
+                if (item.Value > big){
+                    big = item.Value;
+                    key_big = item.Key;
+                }else{
+                    continue;
+                }
             }
+            return key_big;
         }
-        return key_big;
     }
 }
