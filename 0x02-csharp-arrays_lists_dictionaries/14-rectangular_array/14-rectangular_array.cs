1ee9624182c51f3119eac1fb6d5def4bbadb5465
valerc123 <valentina.rua123@gmail.com>
2021-04-23 21:53:25 -0500

    print 2D array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[5, 5];
+        matrix[2, 2] = 1;
+        for (int col = 0; col < 5; col++)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                Console.Write(matrix[col, row]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
