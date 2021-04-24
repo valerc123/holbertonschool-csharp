b8f952f2156345701b7a154133049f62ce087104
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-04-23 19:33:36 -0500

    delete key value of a dictionary

--- 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -5,7 +5,10 @@ class Dictionary
 {
     public static Dictionary<string, string>DeleteKeyValue(Dictionary<string, string> myDict, string key)
     {
-        myDict.Remove(key);
+        if (myDict.ContainsKey(key))
+        {
+            myDict.Remove(key);
+        }
         return myDict;
     }
 }
