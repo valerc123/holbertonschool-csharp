92f532e09f3a7bce1bc7b6ef51f167eaf7add76c
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-04-23 20:16:02 -0500

    best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int big = 0;
+        string key_big = "";
+        foreach (var item in myList)
+        {
+            if (item.Value > big){
+                big = item.Value;
+                key_big = item.Key;
+            }
+        }
+        return key_big;
+    }
+}
