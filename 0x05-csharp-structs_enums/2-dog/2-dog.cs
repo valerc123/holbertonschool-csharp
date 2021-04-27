18d560c1ba2939a084538bb1b6ec01224b910f97
valerc123 <valentina.rua123@gmail.com>
2021-04-26 23:31:06 -0500

    constructor

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,20 @@
+﻿enum Rating {
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+}
