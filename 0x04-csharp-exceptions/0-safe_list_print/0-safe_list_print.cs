f2d2756f91189a9da601b90736937de0015aeaf7
valerc123 <valentina.rua123@gmail.com>
2021-04-26 21:08:23 -0500

    Exceptions

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count;
+        for (count = 0; count < n; count++)
+        {
+            try
+            {
+                Console.WriteLine(myList[count]);
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+               return count;
+            }
+            
+        }
+        return count;
+    }
+}
