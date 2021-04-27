4a8c8fa7d730bf559895da78ce7f0dd18b5cb7d3
valerc123 <valentina.rua123@gmail.com>
2021-04-26 21:32:37 -0500

    divide exception

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int divide = 0;
+        try
+        {
+            divide = a / b;
+        }
+        catch (System.DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally{
+            Console.WriteLine("{0} / {1} = {2}", a, b, divide);
+        }
+    }
+}
