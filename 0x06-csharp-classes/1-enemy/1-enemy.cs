b22f9a9a9e5df07fae102754c54b2bc0f119fab5
valerc123 <valentina.rua123@gmail.com>
2021-04-27 23:22:09 -0500

    Healthy competition

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Represents a enemie </summary>
+    public class Zombie
+    {
+        /// <summary> Represents the health of the Zombie object.</summary>
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class</summary>
+        public Zombie(){
+            this.health = 0;
+        }
+    }
+}
+
