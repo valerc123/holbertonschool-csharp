abb141f5a36e1003ca7a8f639b5eb09cc91e1a33
valerc123 <valentina.rua123@gmail.com>
2021-04-27 23:34:53 -0500

    initialize health

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Represents a Zombie </summary>
+    public class Zombie
+    {
+        /// <summary> Represents the health of the Zombie object.</summary>
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class</summary>
+        /// class and takes <paramref name="value"/></summary>
+        public Zombie(int value = 0){
+            if (value >= 0){
+                this.health = value;
+            }else{
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
+
