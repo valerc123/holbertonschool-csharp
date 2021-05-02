90e46f6faa53f4a66a75197e66047828842daa56
valerc123 <valentina.rua123@gmail.com>
2021-05-02 17:41:09 -0500

    max integer

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class containing a few different algebraic operations.
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Returns the max integer in a list of integers
+        /// </summary>
+        public static int Max(List<int> nums){
+            int max = 0;
+            if (nums.Count > 0){
+                nums.Sort();
+                max = nums[nums.Count - 1];
+            }
+            return max;
+        }
+    }
+}
