32ec5266692fd3dff809f3606b7acd0ac4e615ee
valerc123 <valentina.rua123@gmail.com>
2021-05-02 14:50:13 -0500

    divide matirx

---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Matrix
+    {
+        public static int[,] Divide(int[,] matrix, int num){
+            try
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = matrix[i, j] / num;
+                    }
+                }
+                return matrix;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+        }
+    }
+}
