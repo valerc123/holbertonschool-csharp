f290528bc5311f0a7820c94b2dd52a464b447142
valerc123 <valentina.rua123@gmail.com>
2021-05-02 18:08:41 -0500

    Palindrome

---
+++ 0x07-csharp-tdd/3-palindrome/MyMath/MyMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Class that computes strings
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// return true if string is a palindrome word
+        /// otherwise return false
+        /// </summary>
+        public static bool IsPalindrome(string s){
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
