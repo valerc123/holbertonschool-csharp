18f9b312595498ca326637736387af02ac6434d1
valerc123 <valentina.rua123@gmail.com>
2021-05-02 22:16:42 -0500

    Add tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -10,7 +10,7 @@ namespace MyMathTests
         public void MaxTest()
         {
             var list_test = new List<int>()
-            {90, 10,4,30,88};
+            {10,4,30,88,90};
             int max = Operations.Max(list_test);
             Assert.AreEqual(max, 90);
         }
@@ -28,6 +28,13 @@ namespace MyMathTests
             var list_neg = new List<int>(){-1, -10, -30, -5};
             int neg = Operations.Max(list_neg);
             Assert.AreEqual(neg, -1);
+        }
+           [Test]
+        public void listTest()
+        {
+            var list_neg = new List<int>(){-1, 50, 100, 0, -7};
+            int list = Operations.Max(list_neg);
+            Assert.AreEqual(list, 100);
         }
     }
 }
