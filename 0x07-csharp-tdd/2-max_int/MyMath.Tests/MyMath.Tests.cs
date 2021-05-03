5d0d4802887decf65d7ff8016ca0910c5266c0d5
valerc123 <valentina.rua123@gmail.com>
2021-05-02 21:57:18 -0500

    add test for max integer

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using MyMath;
 
-namespace MyMath.Tests
+namespace MyMathTests
 {
     public class OperationsTests
     {
@@ -13,6 +13,21 @@ namespace MyMath.Tests
             {10,4,30,88};
             int max = Operations.Max(list_test);
             Assert.AreEqual(max, 88);
+        }
+        [Test]
+
+        public void EmptyListTest()
+        {
+            var list_empty = new List<int>();
+            int max = Operations.Max(list_empty);
+            Assert.AreEqual(max, list_empty.Count);
+        }
+        [Test]
+        public void NegNumTest()
+        {
+            var list_neg = new List<int>(){-1, 0, -10, -30, 5};
+            int neg = Operations.Max(list_neg);
+            Assert.AreEqual(neg, list_neg.Count);
         }
     }
 }
