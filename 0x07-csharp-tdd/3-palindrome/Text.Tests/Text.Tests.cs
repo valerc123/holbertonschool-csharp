4755a6ac14193f4beafdf3fabae292bba7b419c1
valerc123 <valentina.rua123@gmail.com>
2021-05-02 23:34:49 -0500

    test

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -9,7 +9,31 @@ namespace TextTests
         [Test]
         public void PalindromeTest()
         {
-            string str = "Racecar";
+            string str = "A man, a plan, a canal: Panama";
+            bool palindrome = Str.IsPalindrome(str);
+            Assert.IsTrue(palindrome);
+        }
+        [Test]
+
+        public void emptyTest()
+        {
+            string str = "";
+            bool palindrome = Str.IsPalindrome(str);
+            Assert.IsTrue(palindrome);
+        }
+         [Test]
+
+        public void middleTest()
+        {
+            string str = "rever";
+            bool palindrome = Str.IsPalindrome(str);
+            Assert.IsTrue(palindrome);
+        }
+        [Test]
+
+        public void uniqueTest()
+        {
+            string str = "ababa";
             bool palindrome = Str.IsPalindrome(str);
             Assert.IsTrue(palindrome);
         }
