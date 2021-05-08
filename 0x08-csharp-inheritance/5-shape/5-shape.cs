a87c53e827fb2d901d5f90ba2692b28eccdc4d51
valerc123 <valentina.rua123@gmail.com>
2021-05-08 18:10:26 -0500

    Shape

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Shape class. Shapely stuff.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// throws a NotImplementedException with message.
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
