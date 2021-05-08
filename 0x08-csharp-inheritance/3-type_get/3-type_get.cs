b48d89a4bb4caa2b3f7a907e8caf2036d543608f
valerc123 <valentina.rua123@gmail.com>
2021-05-08 17:04:10 -0500

    get properties and methods of a object

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        // Printing the properties of the object
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        foreach (var item in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(item.Name);
+        }
+
+        // Printing the methods of the object
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        foreach (var item in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
