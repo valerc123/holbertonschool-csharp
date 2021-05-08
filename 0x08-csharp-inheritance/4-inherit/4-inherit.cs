1f6d0f21afc75f3154e3267112c85cc78a18047c
valerc123 <valentina.rua123@gmail.com>
2021-05-08 17:17:46 -0500

    inherit class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary> Animal class </summary>
+class Animal
+{
+    // Base class
+}
+
+/// <summary> Dog class derived from Animal class </summary>
+class Dog : Animal
+{
+    // Derived class
+}
