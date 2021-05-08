652a7ef3a27b5e5a516bc7e38aa07171e609224f
valerc123 <valentina.rua123@gmail.com>
2021-05-08 16:23:10 -0500

    is instance

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary> Obj class </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if object is an instance of, or an instance of a class
+    /// that inherited from Array.
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
