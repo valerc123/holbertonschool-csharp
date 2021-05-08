c2bf03bf26706532083a02dbc4eb07722beace4d
valerc123 <valentina.rua123@gmail.com>
2021-05-08 15:40:37 -0500

    check if object is int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+/// <summary> Object Class </summary>
+class Obj
+{
+    /// <summary> Check if object is an int </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
