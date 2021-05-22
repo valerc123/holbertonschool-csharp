2c164e4c709e3c3b009553c82defe06e6177a061
valerc123 <valentina.rua123@gmail.com>
2021-05-22 15:07:57 -0500

    magnitude

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 
+/// <summary>
+/// Class VectorMath for Vector Math
+/// </summary>
 class VectorMath
-{
-    public static double Magnitude(double[] vector)
     {
-        
-    }
+        public static double Magnitude(double[] vector)
+        {
+            if (vector.Length == 2)
+            {
+                return (Math.Round(Math.Sqrt((Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2))), 2));
+            }
+            if (vector.Length == 3)
+            {
+                return (Math.Round(Math.Sqrt((Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2))), 2));
+            }
+            return (-1);
+        }
 }
