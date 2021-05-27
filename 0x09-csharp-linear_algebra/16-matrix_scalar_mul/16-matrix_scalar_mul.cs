39b21b10f3d5cf87292041eb5a9b7c5305a3fc1a
valerc123 <valentina.rua123@gmail.com>
2021-05-26 22:24:07 -0500

    matrix scalar mult

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// MatrixMath class
+/// </summary>
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        /// <summary>
+        /// method that multiplies a matrix and a scalar and returns the resulting matrix.
+        /// </summary>
+        double[,] result;
+        double[,] errArr = new double[,] { { -1 } };
+
+        if ((matrix.GetLength(0) == 2) && (matrix.GetLength(1) == 2))
+            result = new double[2, 2];
+        else if ((matrix.GetLength(0) == 3) && (matrix.GetLength(1) == 3))
+            result = new double[3, 3];
+        else
+            return (errArr);
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i, j] = matrix[i, j] * scalar
+        }
+        return result;
+    }
+}
