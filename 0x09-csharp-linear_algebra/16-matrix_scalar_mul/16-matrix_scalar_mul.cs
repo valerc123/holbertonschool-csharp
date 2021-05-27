a33892860dca770b1567332b375647f3b99c6863
valerc123 <valentina.rua123@gmail.com>
2021-05-26 23:14:29 -0500

    try

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -3,23 +3,30 @@
 /// <summary>
 /// MatrixMath class
 /// </summary>
+
 class MatrixMath
 {
+    /// <summary>Function to multiply a matrix by a scalar</summary>
+    /// <param name="matrix">matrix to be multiply</param>
+    /// <param name="scalar">scalar to multiply</param>
+    /// <returns>matrix with result or matrix with -1 if fail</returns>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        /// <summary>
-        /// method that multiplies a matrix and a scalar and returns the resulting matrix.
-        /// </summary>
-        double[,] result = new double[,] { { -1 } };
+        double[,] err = new double[,] { { -1 } };
+
+        if ((matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) || (matrix.GetLength(0) != matrix.GetLength(1)))
+            return (err);
+
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
         //double[,] errArr = new double[,] { { -1 } };
 
-        if ((matrix.GetLength(0) == 2) && (matrix.GetLength(1) == 2))
+        /*if ((matrix.GetLength(0) == 2) && (matrix.GetLength(1) == 2))
             result = new double[2, 2];
         else if ((matrix.GetLength(0) == 3) && (matrix.GetLength(1) == 3))
             result = new double[3, 3];
         else
             return (result);
-
+*/
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
