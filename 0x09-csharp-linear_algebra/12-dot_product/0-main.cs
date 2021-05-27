a87737a09e36a8aa46c7c75e564e0b488305b11f
valerc123 <valentina.rua123@gmail.com>
2021-05-26 21:10:47 -0500

    calculate dot producto of two vectors

---
+++ 0x09-csharp-linear_algebra/12-dot_product/0-main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] vector1 = new double[3] {2, 2, -3};
+        double[] vector2 = new double[3] {4, 1, -3};
+   
+        double result = VectorMath.DotProduct(vector1, vector2);
+        Console.WriteLine("{0}", result);
+    }
+}
---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/// <summary>
+/// VectorMath Class
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// This method calculate dot product of either two 2D or two 3D vectors. 
+    /// </summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == vector2.Length){
+            if(vector1.Length == 2){
+                double dotProduct = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]);
+                return dotProduct;
+            }
+            else if(vector1.Length == 3){
+                double dotProduct = (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + (vector1[2] * vector2[2]);
+                return dotProduct;
+            }
+        }
+        return -1;
+    }
+}
