ef7c1394d25cd3ce77015f31005c66070f522a8c
valerc123 <valentina.rua123@gmail.com>
2021-05-26 21:57:27 -0500

    matrix addition

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Matrix addition
+/// </summary>
+class MatrixMath
+{   /// <summary>
+    /// method that adds two matrices and returns the resulting matrix.
+    /// </summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] result;
+        double[,] errArr = new double[,] { { -1 } };
+
+        if ((matrix1.GetLength(0) == matrix2.GetLength(0)) && (matrix1.GetLength(1) == matrix2.GetLength(1)) && (matrix1.GetLength(0) == 2) && (matrix1.GetLength(1) == 2))
+            result = new double[2, 2];
+        else if ((matrix1.GetLength(0) == matrix2.GetLength(0)) && (matrix1.GetLength(1) == matrix2.GetLength(1)) && (matrix1.GetLength(0) == 3) && (matrix1.GetLength(1) == 3))
+            result = new double[3, 3];
+        else
+            return (errArr);
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return result;
+    }
+}
