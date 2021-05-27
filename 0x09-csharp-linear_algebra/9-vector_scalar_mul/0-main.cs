b8c74a018667278779c8bc4e45e084130fdefe12
valerc123 <valentina.rua123@gmail.com>
2021-05-26 19:13:23 -0500

    multiply vector for scalar

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/0-main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] vector = {2, 2, -3};
+        double scalar = 0.5;
+        double[] result = VectorMath.Multiply(vector, scalar);
+        foreach (var item in result)
+        {
+            Console.WriteLine("{0}", item);
+        }
+    }
+}
---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Vector Math class
+/// <summary>
+class VectorMath
+{
+    /// <summary>
+    /// This method multiply a vector and a scalar and returns the resulting vector.
+    /// <summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] result = new double[1] { -1 };
+        if (vector.Length == 2){
+            result = new double[2] {vector[0] * scalar, vector[1] * scalar};
+        }
+        else if (vector.Length == 3)
+        {
+            result = new double[3] {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};  
+        }
+
+        return (result);        
+    }
+}
