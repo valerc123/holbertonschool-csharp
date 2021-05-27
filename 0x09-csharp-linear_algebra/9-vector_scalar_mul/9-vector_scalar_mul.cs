94bf2d3dda16a6ea399e0cf8464290b810027f8e
valerc123 <valentina.rua123@gmail.com>
2021-05-26 19:16:46 -0500

    fix return

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -2,23 +2,25 @@
 
 /// <summary>
 /// Vector Math class
-/// <summary>
+/// </summary>
 class VectorMath
 {
     /// <summary>
     /// This method multiply a vector and a scalar and returns the resulting vector.
-    /// <summary>
+    /// </summary>
     public static double[] Multiply(double[] vector, double scalar)
     {
         double[] result = new double[1] { -1 };
         if (vector.Length == 2){
             result = new double[2] {vector[0] * scalar, vector[1] * scalar};
+            return (result);
         }
         else if (vector.Length == 3)
         {
-            result = new double[3] {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};  
+            result = new double[3] {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};
+            return (result);
         }
 
-        return (result);        
+        return (result);
     }
 }
