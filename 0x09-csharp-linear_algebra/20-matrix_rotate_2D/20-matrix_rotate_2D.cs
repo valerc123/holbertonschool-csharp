913ba74faa28000e11806132d5dfd2b02a954943
valerc123 <valentina.rua123@gmail.com>
2021-05-30 21:05:20 -0500

    matrix rotation

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MatrixMath
+{
+    /// <summary>Funtion to rotate a matrix</summary>
+    /// <param name="matrix">matrix</param>
+    /// <param name="angle">angle</param>
+    /// <returns>matrix with result or matrix with -1 if fail</returns>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] wrong = new double[,] { { -1 } };
+        double[,] matrix1 = new double[,] {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}};
+        double oper;
+        double[,] res = new double[2, 2];
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return wrong;
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+            {
+                oper = 0;
+                for (int k = 0; k < 2; k++)
+                    oper = Math.Round(oper + (matrix[i, k] * matrix1[k, j]), 2);
+                res[i, j] = oper;
+            }
+
+        return res;
+    }
+}
