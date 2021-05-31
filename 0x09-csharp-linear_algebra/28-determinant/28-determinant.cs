c8c6dbc1e4f7a956cbb5afa179f40af524591b2d
valerc123 <valentina.rua123@gmail.com>
2021-05-30 23:08:11 -0500

    determinant

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MatrixMath 
+{
+    /// <summary>Function to find the determinant</summary>
+    /// <param name="matrix">the matrix</param>
+    /// <returns>the determinant or -1 if fail</returns>
+    public static double Determinant(double[,] matrix) {
+        double deter = 0;
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) {
+            deter = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
+            return(deter);
+        } else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3) {
+            deter = Math.Round((matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]))), 2);
+            return(deter);
+        } else
+            return -1;
+    }
+}
