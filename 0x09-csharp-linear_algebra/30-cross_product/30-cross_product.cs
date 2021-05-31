c5aaef42c9b76b44f07ff6806792f1b5ed50b48b
valerc123 <valentina.rua123@gmail.com>
2021-05-30 23:10:17 -0500

    cross product

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class VectorMath
+{
+    /// <summary>Function to find cross product</summary>
+    /// <param name="vector1">first vector</param>
+    /// <param name="vector2">second vector</param>
+    /// <returns>crossproduct or -1 if fail</returns>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        double[] wrong = new double[]{-1};
+        double[] res;
+        if (vector1.Length != 3 || vector2.Length != 3)
+            return wrong;
+        res = new double[vector1.Length];
+        res[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+        res[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
+        res[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+        return res;
+    }
+}
