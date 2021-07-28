4db16d504703d3de24442ad0820e64c37bb7e167
valerc123 <valentina.rua123@gmail.com>
2021-07-27 23:35:23 -0500

    class queu

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -3,7 +3,8 @@
 /// <summary>
 /// Generic class queue
 /// </summary>
-public class Queue<T>
+/// <typeparam name="T">The type the Queue contains</typeparam>
+class Queue<T>
 {
   /// <summary> Checks if input is queue type <summary>
   public Type CheckType(){
