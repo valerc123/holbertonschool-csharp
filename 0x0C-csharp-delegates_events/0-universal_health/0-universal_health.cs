7d99f822f98db9473a599f1a3db30bebb2da6ed0
valerc123 <valentina.rua123@gmail.com>
2021-07-29 21:17:49 -0500

    universla health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Creates a public Player class
+/// </summary>
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Player Constructor
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f){
+        this.name = name;
+        if(maxHp <= 0f){
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    
+    /// <summary>
+    /// Display current health
+    /// </summary>
+    public void PrintHealth(){
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+}
