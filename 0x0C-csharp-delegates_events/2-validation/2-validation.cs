5537ebd2f8cad908a7ecb211911db9aacfd63a95
valerc123 <valentina.rua123@gmail.com>
2021-07-29 22:23:41 -0500

    validation

---
+++ 0x0C-csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+/// <summary>
+/// Creates a public Player class
+/// </summary>
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Player Constructor
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f){
+        this.name = name;
+        if(maxHp <= 0f){
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    
+    /// <summary>
+    /// Display current health
+    /// </summary>
+    public void PrintHealth(){
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+
+    private delegate void CalculateHealth(float amount);
+
+    /// <summary>
+    /// Applies damage to player.
+    /// </summary>
+    public void TakeDamage(float damage){
+        if(damage <= 0){
+            Console.WriteLine("{0} takes 0 damage!", this.name);
+        }else{
+            ValidateHP(this.hp - damage);
+            Console.WriteLine("{0} takes {1} damage!", this.name, damage);
+        }
+    }
+
+    /// <summary>
+    /// Rejuvinates player HP.
+    /// </summary>
+    public void HealDamage(float heal){
+        if(heal <= 0){
+            Console.WriteLine("{0} heals 0 HP!", this.name);
+        }else{
+            ValidateHP(this.hp + heal);
+            Console.WriteLine("{0} heals {1} HP!", this.name, heal);
+        }
+    }
+
+    /// <summary>
+    /// Validates player's new HP.
+    /// </summary>
+    public void ValidateHP(float newHp) {
+        if (newHp <=0) {
+            this.hp = 0;
+        }
+        else if (newHp > maxHp) {
+            this.hp = maxHp;
+        }
+        else {
+            this.hp = newHp;
+        }
+    }
+}
