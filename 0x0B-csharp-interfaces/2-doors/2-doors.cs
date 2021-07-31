77f18f5c7cbc106723ea803b30a4c70307fb666e
valerc123 <valentina.rua123@gmail.com>
2021-07-31 01:30:17 -0500

    door class

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+/// <summary>
+/// Provides an abstract base class.
+/// </summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    /// <summary>
+    /// Gets the string representation of the instance.
+    /// </summary>
+    /// <returns>The string representation.</returns>
+    public override string ToString()
+    {
+        return String.Format("{0} is a {1}", name, GetType());
+    }
+}
+
+/// <summary>
+/// Defines the properties of an interactive object
+/// </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// Defines the properties of a breakable object
+/// </summary>
+interface IBreakable
+{
+    int durability { get; set;}
+    void Break();
+}
+
+/// <summary>
+/// Defines the properties of a collectable object
+/// </summary>
+interface ICollectable
+{
+    bool isCollected { get; set;}
+    void Collect();
+}
+
+/// <summary>
+/// Represents a Door.
+/// </summary>
+class Door : Base, IInteractive
+{
+    /// <summary>
+    /// Sets the name of the Door.
+    /// </summary>
+    public Door(string door = "Door"){
+        this.name = door;
+    }
+
+    /// <summary>
+    /// Prints interaction string.
+    /// </summary>
+    public void Interact() {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
