31bef60bb4b2c97bb6977a11e91a96d9e8b13af2
valerc123 <valentina.rua123@gmail.com>
2021-07-31 00:44:43 -0500

    abstract class

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Provides an abstract base class.
+/// </summary>
+abstract class Base
+{
+    public string name;
+
+    /// <summary>
+    /// Gets the string representation of the instance.
+    /// </summary>
+    /// <returns>The string representation.</returns>
+    public override string ToString(){
+        return String.Format("{0} is a {1}", name, GetType());
+    }
+}
