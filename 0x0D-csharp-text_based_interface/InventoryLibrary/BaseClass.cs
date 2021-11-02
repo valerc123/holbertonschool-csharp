08116a2949f92616006e7863f6ad175b54f7eed2
valerc123 <valentina.rua123@gmail.com>
2021-11-01 19:35:47 -0500

    define id, start date, update date

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <summary> BaseClass to inherit from </summary>
+    public class BaseClass
+    {
+        /// <summary> id of the object </summary>
+        public string id{get; set;}
+        /// <summary> date when object was created </summary>
+        public DateTime date_created{get; set;}
+        /// <summary> date when object was updated </summary>
+        public DateTime date_updated{get; set;}
+    }
+}
