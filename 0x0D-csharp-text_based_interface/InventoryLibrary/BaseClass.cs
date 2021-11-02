2268f514a5bb668b0124ccfc0c3dcdbf0af17d70
valerc123 <valentina.rua123@gmail.com>
2021-11-01 20:06:31 -0500

    inventory

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 
-namespace InventoryLibrary
+/// <summary> BaseClass to inherit from </summary>
+public class BaseClass
 {
-    /// <summary> BaseClass to inherit from </summary>
-    public class BaseClass
+    /// <summary> id of the object </summary>
+    public string id{get; set;}
+    /// <summary> date when object was created </summary>
+    public DateTime date_created{get; set;}
+    /// <summary> date when object was updated </summary>
+    public DateTime date_updated{get; set;}
+
+    ///<summary>Base Class constructor.</summary>
+    public BaseClass()
     {
-        /// <summary> id of the object </summary>
-        public string id{get; set;}
-        /// <summary> date when object was created </summary>
-        public DateTime date_created{get; set;}
-        /// <summary> date when object was updated </summary>
-        public DateTime date_updated{get; set;}
+        this.id = System.Guid.NewGuid().ToString();
+        this.date_created = DateTime.Now;
+        this.date_updated = DateTime.Now;
     }
 }
